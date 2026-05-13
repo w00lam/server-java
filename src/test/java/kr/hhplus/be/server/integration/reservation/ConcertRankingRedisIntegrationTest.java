4a4a807c60abb82e63ec29930c384ef169930fa2
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-05-13 11:15:42 +0900

    refactor: clarify concert ranking integration test

--- src/test/java/kr/hhplus/be/server/integration/reservation/ConcertRankingRedisIntegrationTest.java
+++ src/test/java/kr/hhplus/be/server/integration/reservation/ConcertRankingRedisIntegrationTest.java
@@ -5,6 +5,7 @@ import kr.hhplus.be.server.concert.application.service.ConcertRankingItem;
 import kr.hhplus.be.server.integration.ReservationIntegrationTestBase;
 import kr.hhplus.be.server.payment.domain.model.PaymentMethod;
 import kr.hhplus.be.server.user.domain.model.User;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,20 +14,25 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConcertRankingRedisIntegrationTest extends ReservationIntegrationTestBase {
+    private static final String RANKING_CONCERT_TITLE = "Redis Ranking Concert";
+
     @Autowired
     private ConcertRankingRepositoryPort concertRankingRepository;
 
     @Test
-    void confirmedPaymentReflectsConcertRankingInRedis() {
+    @DisplayName("결제가 완료되면 콘서트 예약 랭킹에 반영된다")
+    void paidReservationIncreasesConcertRanking() {
         User user = createUserWithPoints(10_000);
-        ReservedSeat reservedSeat = createReservedSeat(user, "ranking test concert");
+        ReservedSeat reservedSeat = createReservedSeat(user, RANKING_CONCERT_TITLE);
 
         payReservation(reservedSeat.reservationId(), 5_000, PaymentMethod.CARD);
 
         List<ConcertRankingItem> rankings = concertRankingRepository.findTopRanked(10);
 
         assertThat(rankings)
-                .extracting(ConcertRankingItem::concertId)
-                .contains(reservedSeat.concertId());
+                .anySatisfy(ranking -> {
+                    assertThat(ranking.concertId()).isEqualTo(reservedSeat.concertId());
+                    assertThat(ranking.reservationCount()).isEqualTo(1L);
+                });
     }
 }
